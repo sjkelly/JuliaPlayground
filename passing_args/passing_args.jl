11197571c58592f8da125229595f5fdf63ad5606
Steve Kelly <kd2cca@gmail.com>
2014-06-18 18:13:58 -0400

    working version, thanks to leah hanson https://groups.google.com/forum/#!topic/julia-users/aRo-eZWATOw

--- passing_args/passing_args.jl
+++ passing_args/passing_args.jl
@@ -14,7 +14,7 @@ function arg_collector(;args...)
     # do some operation on args
     println(args)
     println(typeof(args))
-    print_args(args)
+    print_args(;args...)
 end
 
 arg_collector(x=5, y=6, z=7)
